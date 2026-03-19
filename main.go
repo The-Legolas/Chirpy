e97600432cc905bc046f3e42ddf54593340afec5
Legolas <The-Legolas>
2026-03-19 17:32:21 +0100

    Created a primitive local server which currently only prints welcome msg the base url and has an image in another assets part of the url.

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error listening to server: %v\n", err)
+		return
+	}
+
+}
