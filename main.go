7a3a05c6e21942b9bb25d5fb5a9ca70c01496a59
Legolas <The-Legolas>
2026-03-25 20:03:29 +0100

    Implemented the next part of the database layer for chirps. Added handlerChirpsCreate to handle chirp creation, including validation and persistence to the database.
    Removed handler_validate.go and moved its validation logic into
    handler_chirps_create.go, which now serves as its replacement.
    
    Introduced new database schema via 002_chirps.sql and corresponding
    queries in sqlc chirps.sql.

--- main.go
+++ main.go
@@ -52,7 +52,8 @@ func main() {
 	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	mux.Handle("/app/", fsHandler)
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
